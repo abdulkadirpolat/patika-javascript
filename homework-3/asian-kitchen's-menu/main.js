abde7a28a122ea3e07f3b1bb1f81bc33f8678373
Abdulkadir Polat <tr.abdulkadir.polat@gmail.com>
2021-08-30 17:18:01 +0300

    fixes

---
+++ homework-3/asian-kitchen's-menu/main.js
@@ -0,0 +1,59 @@
+const menuContainer = document.querySelector(".menu-container");
+const buttons = document.querySelector(".buttons");
+
+fetch("asian_kichen.json")
+  .then((response) => response.json())
+  .then((json) => {
+    return jsonData(json), loadedData(json);
+  })
+  .catch((error) => console.log(error));
+
+function items(item) {
+  let menuItem = document.createElement("div");
+  menuItem.className = "menu-item";
+  menuItem.innerHTML = 
+  `<div class="card-image"> <img class="card-img"src="${item.img}"/>
+  </div>
+  <div class="menu-info">
+    <div class="menu-title">
+      <h2>${item.title}</h2>
+      <h2>${item.price}</h2>
+    </div>
+    <div class="about">${item.desc}</div>
+  </div>`;
+  menuContainer.append(menuItem);
+}
+
+function loadedData(e) {
+  e.map((item) => {
+    return items(item);
+  });
+}
+
+function deleteItems() {
+  menuContainer.innerHTML = "";
+}
+
+function jsonData(asianData) {
+  buttons.addEventListener("click", (e) => {
+    let clickId = e.target.id;
+
+    if (e.target.className == "buttons") {
+      return;
+    } else if (clickId == "all") {
+      deleteItems();
+      asianData.map((item) => {
+        return items(item);
+      });
+    } else if (clickId === "korea" || "japan" || "china") {
+      deleteItems();
+      asianData
+        .filter(
+          (f) => f.category == `${clickId[0].toUpperCase()}${clickId.slice(1)}`
+        )
+        .map((item) => {
+          return items(item);
+        });
+    }
+  });
+}
